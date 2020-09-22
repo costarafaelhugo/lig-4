2a40ebca3974b205937cd07de51f98af28751d0f
Isabella Pegoraro Macondes <isabella.pegorarom@gmail.com>
2020-09-22 16:24:27 -0300

    Botão iniciar e vez do jogador

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ function makeBoard() {
         ["c", "c", "c", "c", "c", "c", "c"],
         ["c", "c", "c", "c", "c", "c", "c"]
     ]
-    
-    
-    for(let i = 0; i < board.length; i++){
-        for (let j = 0; j < board[i].length; j++){
-            if (j === 0){
+
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (j === 0) {
                 let tabuleiro = document.getElementById('board')
                 let column = document.createElement('div')
                 column.classList.add('column')
                 column.id = `${i} ${j}`
                 tabuleiro.appendChild(column)
             }
-            if (j > 0){
+            if (j > 0) {
                 let column = document.getElementById(`${i} 0`)
                 let cell = document.createElement('div')
                 cell.classList.add('cell')
@@ -36,27 +36,73 @@ addEventLitners()
 
 function addEventLitners() {
     let column = document.getElementsByClassName('column')
-    for(let i = 0; i < column.length; i++){
+    for (let i = 0; i < column.length; i++) {
         column[i].addEventListener('click', creatSon)
     }
 }
 
-function creatSon(u){
+
+let currentPlayer = 0
+
+function creatSon(u) {
     u.stopPropagation()
     let column = u.target.parentNode
     let cells = column.children
-    console.log(cells)
     peace = document.createElement('div')
-    peace.classList.add('rick')
+    playersTurn(rick)
+    if (currentPlayer === 0) {
+        playersTurn(morty)
+        peace.classList.add('rick')
+        let rickImg = document.createElement('img')
+        rickImg.src = 'imagens/rick.png'
+        peace.appendChild(rickImg)
+        currentPlayer++
+    }
+    else {
+        peace.classList.add('morty')
+        let mortyImg = document.createElement('img')
+        mortyImg.src = 'imagens/morty.png'
+        peace.appendChild(mortyImg)
+        currentPlayer--
+    }
     let lastCellViable = null
-    for(let i = 0; i < cells.length; i++){
-        if (cells[i].childElementCount === 0){
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].childElementCount === 0) {
             lastCellViable = cells[i]
             break
         }
     }
-
     lastCellViable.appendChild(peace)
-    console.log(lastCellViable)
-    
+}
+
+
+function play(){
+    let button = document.getElementById('jogar')
+    button.addEventListener('click', function(){
+        let hidden = document.getElementById('inicial')
+        hidden.classList.add('hide') 
+
+        let board = document.getElementById('board')
+        board.classList.remove('boardHide')
+
+        let divPlayer = document.getElementById('currentPlayer')
+        divPlayer.classList.remove('boardHide')
+
+        playersTurn(rick)
+    })
+}
+
+play()
+
+function playersTurn(a){
+    let output = document.getElementById('currentPlayer')
+    if (a === rick){
+        let player1 = document.getElementById('player1').value
+        output.innerText = `Sua vez ${player1}!`
+    }
+
+    if (a === morty){
+        let player2 = document.getElementById('player2').value
+        output.innerText = `Sua vez ${player2}!`
+    }
 }
