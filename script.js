800d42153756db90231612cc553fcc1e2439df36
VFagionato <viktormfagionato@gmail.com>
2020-09-23 14:34:27 -0300

    Adicinado função que tem todas as condições de vitória

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function makeBoard() {
     board = [
-        ["c", "c", "c", "c", "c", "c", "c"],
-        ["c", "c", "c", "c", "c", "c", "c"],
-        ["c", "c", "c", "c", "c", "c", "c"],
-        ["c", "c", "c", "c", "c", "c", "c"],
-        ["c", "c", "c", "c", "c", "c", "c"],
-        ["c", "c", "c", "c", "c", "c", "c"],
-        ["c", "c", "c", "c", "c", "c", "c"]
+        ["C", "c", "c", "c", "c", "c", "c"],
+        ["C", "c", "c", "c", "c", "c", "c"],
+        ["C", "c", "c", "c", "c", "c", "c"],
+        ["C", "c", "c", "c", "c", "c", "c"],
+        ["C", "c", "c", "c", "c", "c", "c"],
+        ["C", "c", "c", "c", "c", "c", "c"],
+        ["C", "c", "c", "c", "c", "c", "c"]
     ]
 
 
@@ -50,21 +50,6 @@ function creatSon(u) {
     let cells = column.children
     peace = document.createElement('div')
     playersTurn(rick)
-    if (currentPlayer === 0) {
-        playersTurn(morty)
-        peace.classList.add('rick')
-        let rickImg = document.createElement('img')
-        rickImg.src = 'imagens/rick.png'
-        peace.appendChild(rickImg)
-        currentPlayer++
-    }
-    else {
-        peace.classList.add('morty')
-        let mortyImg = document.createElement('img')
-        mortyImg.src = 'imagens/morty.png'
-        peace.appendChild(mortyImg)
-        currentPlayer--
-    }
     let lastCellViable = null
     for (let i = 0; i < cells.length; i++) {
         if (cells[i].childElementCount === 0) {
@@ -72,12 +57,27 @@ function creatSon(u) {
             break
         }
     }
-    lastCellViable.appendChild(peace)
-    modifyBoard(peace)
-    seekVertical(peace)
-    seekHorizontal(peace)
-    seekDiagonalDownRight(peace)
-    seekDiagonalDownLeft(peace)
+    if (currentPlayer === 0) {
+        playersTurn(morty)
+        peace.classList.add('rick')
+        let rickImg = document.createElement('img')
+        rickImg.src = 'imagens/rick.png'
+        peace.appendChild(rickImg)
+        lastCellViable.appendChild(peace)
+        modifyBoard(peace)
+        seekAndDestroy(peace)
+        currentPlayer++
+    }
+    else {
+        peace.classList.add('morty')
+        let mortyImg = document.createElement('img')
+        mortyImg.src = 'imagens/morty.png'
+        peace.appendChild(mortyImg)
+        lastCellViable.appendChild(peace)
+        modifyBoard(peace)
+        seekAndDestroy(peace)
+        currentPlayer--
+    }
 }
 
 
@@ -94,6 +94,7 @@ button.addEventListener('click', function () {
     divPlayer.classList.remove('boardHide')
 
     playersTurn(rick)
+    
 })
 
 function playersTurn(a) {
@@ -123,91 +124,78 @@ function modifyBoard(letter) {
 
 
 
-function seekVertical(letter) {
-    const edgeX = board[0].length - 3
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < edgeX; j++) {
-            if (letter.classList.value === "rick") {
-                let cell = "R"
-                if (cell === board[i][j + 1] && cell === board[i][j + 2] && cell === board[i][j + 3] && cell === board[i][j + 4]) {
-                    console.log('WIN')
-                    break
+function seekAndDestroy(letter) {
+    let edgeI = board.length
+    let edgeJ = board[0].length - 3
+
+    //vertical linha por linha
+    for(let i = 0; i < edgeI; i++){
+        for(j = 1; j < edgeJ; j++){
+            let cell = board[i][j]
+            if(cell !== "c"){
+                if(cell === board[i][j+1] && cell == board[i][j+2] && cell === board[i][j+3]){
+                    console.log(`${currentPlayer} VERTICAL WIN`)
+                    return true
                 }
             }
-            if (letter.classList.value === "morty") {
-                let cell = "M"
-                if (cell === board[i][j + 1] && cell === board[i][j + 2] && cell === board[i][j + 3] && cell === board[i][j + 4]) {
-                    console.log('WIN')
-                    break
+        }
+    }
+
+    //horizontal
+    edgeI = board.length - 3
+    edgeJ = board[0].length
+    for(let i = 0; i < edgeI ; i++){
+        for(let j = 1; j < edgeJ; j++){
+            let cell = board[i][j]
+            if(cell !== "c"){
+                if(cell === board[i+1][j] && cell === board[i+2][j] && cell === board[i+3][j]){
+                    console.log(`${currentPlayer} HORIZONTAL WIN`)
+                    return true
+                }
+            }
+        }
+    }
+
+    //diagonalUpRight
+    edgeI = board.length - 3
+    edgeJ = board[0].length -3
+    for(let i = 0; i < edgeI; i++){
+        for(let j = 1; j < edgeJ; j++){
+            let cell = board[i][j]
+            if(cell !== "c"){
+                if(cell === board[i+1][j+1] && cell === board[i+2][j+2] && cell === board[i+3][j+3]){
+                    console.log(`${currentPlayer} DiagonalUpRight WIN`)
+                    return true
+                }
+            }
+        }
+    }
+
+    edgeI = board.length
+    for(let i = 3; i < edgeI; i++){
+        for(let j = 1; j < edgeJ; j++){
+            let cell = board[i][j]
+            if(cell !== "c"){
+                if(cell === board[i-1][j+1] && cell === board[i-2][j+2] && cell === board[i-3][j+3]){
+                    console.log(` ${currentPlayer} DiagonalLeft WIN`)
+                    return true
                 }
             }
         }
     }
 }
 
-function seekHorizontal(letter) {
-    const edgeY = board.length - 3
-    for (let i = 0; i < edgeY; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            if (letter.classList.value === "rick") {
-                let cell = "R"
-                if (cell === board[i + 1][j] && cell === board[i + 2][j] && cell === board[i + 3][j] && cell === board[i + 4][j]) {
-                    console.log('WIN')
-                    break
-                }
-            }
-            if (letter.classList.value === "morty") {
-                let cell = "M"
-                if (cell === board[i + 1][j] && cell === board[i + 2][j] && cell === board[i + 3][j] && cell === board[i + 4][j]) {
-                    console.log('WIN')
-                    break
-                }
-            }
-        }
-    }
-}
 
-function seekDiagonalDownRight(letter) {
-    let edgeX = board[0].length - 4
-    for (let i = 2; i < board.length; i++) {
-        for (let j = 0; j < edgeX; j++) {
-            if (letter.classList.value === "rick") {
-                let cell = "R"
-                if (cell === board[i - 1][j + 1] && cell === board[i - 2][j + 2] && cell === board[i - 3][j + 3] && cell === board[i - 4][j + 4]) {
-                    console.log('WIN')
-                    break
-                }
-            }
-            if (letter.classList.value === "morty") {
-                let cell = "M"
-                if (cell === board[i - 1][j + 1] && cell === board[i - 2][j + 2] && cell === board[i - 3][j + 3] && cell === board[i - 4][j + 4]) {
-                    console.log('WIN')
-                    break
-                }
-            }
-        }
-    }
-}
 
-function seekDiagonalDownLeft(letter) {
-    let edgeY = board.length - 4
-    let edgeX = board[0].length - 4
-    for (let i = 0; i < edgeY; i++) {
-        for (let j = 0; j < edgeX; j++) {
-            if (letter.classList.value === "rick") {
-                let cell = "R"
-                if (cell === board[i + 1][j + 1] && cell === board[i + 2][j + 2] && cell === board[i + 3][j + 3] && cell === board[i + 4][j + 4]) {
-                    console.log('WIN')
-                    break
-                }
-            }
-            if (letter.classList.value === "morty") {
-                let cell = "M"
-                if (cell === board[i + 1][j + 1] && cell === board[i + 2][j + 2] && cell === board[i + 3][j + 3] && cell === board[i + 4][j + 4]) {
-                    console.log('WIN')
-                    break
-                }
-            }
-        }
-    }
-}
+
+
+
+// board = [
+// 0   ["C", "c", "c", "c", "c", "c", "c"],
+// 1   ["C", "c", "c", "c", "c", "c", "c"],
+// 2   ["C", "c", "c", "c", "c", "c", "c"],
+// 3   ["C", "c", "c", "c", "c", "c", "c"],
+// 4   ["C", "c", "c", "c", "c", "c", "c"],
+// 5   ["C", "c", "c", "c", "c", "c", "c"],
+// 6   ["C", "c", "c", "c", "c", "c", "c"]
+// ]
