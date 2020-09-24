2bca8e47c5f6e897400f6e577a5ad0f077b92dd2
Isabella Pegoraro Macondes <isabella.pegorarom@gmail.com>
2020-09-24 09:25:45 -0300

    Começando refatoração

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//FUNÇÃO DE CRIAÇÃO DO TABULEIRO
+
 function makeBoard() {
     board = [
         ["C", "c", "c", "c", "c", "c", "c"],
@@ -32,6 +34,9 @@ function makeBoard() {
 
 makeBoard()
 
+
+//FUNÇÃO QUE COLOCA UM ADDEVENTLISTNERS EM CADA COLUNA
+
 addEventLitners()
 
 function addEventLitners() {
@@ -41,6 +46,7 @@ function addEventLitners() {
     }
 }
 
+//FUNÇÃO QUE CRIA AS PEÇAS DOS JOGADORES
 
 let currentPlayer = 0
 
@@ -59,56 +65,50 @@ function creatSon(u) {
     }
     if (currentPlayer === 0) {
         playersTurn(morty)
+        //acho que essa parte de adicionar a imagem e criar a div em si poderia ser uma função
         peace.classList.add('rick')
         let rickImg = document.createElement('img')
         rickImg.src = 'imagens/rick.png'
         peace.appendChild(rickImg)
         lastCellViable.appendChild(peace)
+        //até aqui 
         modifyBoard(peace)
         if (seekAndDestroy()) {
+            //isso aqui poderia ser outra função também, pois até no empate a gnt faz isso 
             let output = document.getElementById('currentPlayer')
             let player1 = document.getElementById('player1').value
             output.innerText = `Parabéns ${player1}, você venceu! Um Rick sempre vai ser melhor que um Morty...`
+            //até aqui
         }
         currentPlayer++
     }
     else {
+        //mesma coisa, chamar função para criar a div
         peace.classList.add('morty')
         let mortyImg = document.createElement('img')
         mortyImg.src = 'imagens/morty.png'
         peace.appendChild(mortyImg)
         lastCellViable.appendChild(peace)
+        //até aqui
         modifyBoard(peace)
         if (seekAndDestroy()) {
+            //outra função
             let output = document.getElementById('currentPlayer')
             let player2 = document.getElementById('player2').value
             output.innerText = `Parabéns ${player2}, você venceu! As vezes um Morty consegue ser melhor...`
+            //até aqui
         }
 
         currentPlayer--
     }
     if (isDraw()) {
+        //aqui seria a mesma função da condição de vitória, é só a gnt fazer um parâmetro que iria identificar o que fazer no innerText
         let output = document.getElementById('currentPlayer')
         output.innerText = "Deu empate!"
     }
 }
 
-console.log(board)
-
-
-
-function isDraw() {
-    let result = true
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 1; j < board[i].length; j++) {
-            if (board[i][j] === "c") {
-                result = false
-            }
-        }
-    }
-    return result
-}
-
+//FUNÇÃO DO BOTÃO JOGAR
 
 
 let button = document.getElementById('jogar')
@@ -126,6 +126,8 @@ button.addEventListener('click', function () {
 
 })
 
+//FUNÇÃO QUE IDENTIFICA DE QUAL PLAYER É A VEZ
+
 function playersTurn(a) {
     let output = document.getElementById('currentPlayer')
     if (a === rick) {
@@ -139,6 +141,8 @@ function playersTurn(a) {
     }
 }
 
+//FUNÇÃO QUEMODIFICA O BOARD CADA VEZ QUE UMA PEÇA É COLOCADA
+
 function modifyBoard(letter) {
     let arrId = letter.parentNode.id.split('')
     if (letter.classList.value === "rick") {
@@ -150,8 +154,8 @@ function modifyBoard(letter) {
 }
 
 
-
-
+//FUNÇÃO DE CONDIÇÃO DE VITÓRIA
+//Dividir seekAndDestroy em funções menores, achar o bug na função de diagonal
 
 function seekAndDestroy() {
     let edgeI = board.length
@@ -214,17 +218,17 @@ function seekAndDestroy() {
     }
 }
 
+//FUNÇÃO DE EMPATE
 
+function isDraw() {
+    let result = true
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 1; j < board[i].length; j++) {
+            if (board[i][j] === "c") {
+                result = false
+            }
+        }
+    }
+    return result
+}
 
-
-
-
-// board = [
-// 0   ["C", "c", "c", "c", "c", "c", "c"],
-// 1   ["C", "c", "c", "c", "c", "c", "c"],
-// 2   ["C", "c", "c", "c", "c", "c", "c"],
-// 3   ["C", "c", "c", "c", "c", "c", "c"],
-// 4   ["C", "c", "c", "c", "c", "c", "c"],
-// 5   ["C", "c", "c", "c", "c", "c", "c"],
-// 6   ["C", "c", "c", "c", "c", "c", "c"]
-// ]
